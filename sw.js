067a5b0431b84ecbe3d5d04a15e5935476ff46b2
bodhiintellect <nath.syam.1986@gmail.com>
2026-03-03 17:11:11 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,44 @@
-const CACHE_NAME = 'chakra-v1';
+const CACHE_NAME = 'chakra-v2';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/app.js',
-  '/manifest.json',
-  '/symbols/root.png',
-  '/symbols/sacral.png',
-  '/symbols/solar.png',
-  '/symbols/heart.png',
-  '/symbols/throat.png',
-  '/symbols/thirdeye.png',
-  '/symbols/crown.png'
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  './scripts.json',
+  './symbols/root.png',
+  './symbols/sacral.png',
+  './symbols/solar.png',
+  './symbols/heart.png',
+  './symbols/throat.png',
+  './symbols/thirdeye.png',
+  './symbols/crown.png'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => {
+      // Use relative paths to support GitHub Pages sub-folders
+      return cache.addAll(ASSETS);
+    })
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Clean up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
   );
 });
