362f1e967dc60212d2233a2763e5d39fb8c1c29f
bodhiintellect <nath.syam.1986@gmail.com>
2026-05-25 22:04:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1158,7 +1158,7 @@ class MeditationController {
         await this.narrate(this.scripts.bath_session.instructions[state.language], false);
 
         let remaining = state.timeBath;
-        const reminderSecond = 60;
+        const reminderSecond = 180;
 
         while (remaining > 0) {
             if (!this.isMeditationActive) return;
